b213e939ecd7b1d5fcc2910e54f58b241be5a007
Brandon Rubell <73529325+Rubelefsky@users.noreply.github.com>
2026-02-22 06:47:57 -0500

    Update API_URL to use relative path

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ if (heroBadge) {
 // IMPORTANT: Replace this URL with your actual Azure Function URL
 // After deploying your function, it will look something like:
 // https://cloudresume-api.azurewebsites.net/api/GetVisitorCount
-const API_URL = 'https://cloudresume-api-rubelefsky-gceag4fsg8gmh0d5.centralus-01.azurewebsites.net/api/getvisitorcounter';
+const API_URL = '/api/GetVisitorCount';
 
 /**
  * Fetches the visitor count from the Azure Function API
